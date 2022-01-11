88d2bb05db46cd0f6d9181089691aa61ccdbdcf7
Gabriel-Paulucci <gabrielpauluccimunizmendes@gmail.com>
2022-01-11 19:15:00 -0300

    Tah .... so n aparece

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 pub mod terrain;
 
 use bevy::{prelude::*, DefaultPlugins};
-use terrain::gen_perlin_noise;
+use terrain::gen_terrain;
 
 fn main() {
-    // App::new()
-    //     .insert_resource(Msaa { samples: 4 })
-    //     .add_plugins(DefaultPlugins)
-    //     .run();
-
-    gen_perlin_noise();
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(gen_terrain)
+        .run();
 }
--- src/terrain.rs
+++ src/terrain.rs
@@ -1,15 +1,86 @@
+use bevy::{
+    prelude::*,
+    render::{
+        mesh::{Indices, VertexAttributeValues},
+        render_resource::PrimitiveTopology::TriangleList,
+    },
+};
 use noise::{
-    utils::{NoiseMapBuilder, PlaneMapBuilder},
-    Fbm, Perlin,
+    utils::{NoiseMap, NoiseMapBuilder, PlaneMapBuilder},
+    Fbm,
 };
 
-pub fn gen_perlin_noise() {
-    let perlin = Fbm::default();
+#[derive(Component)]
+pub struct Terrain;
 
-    PlaneMapBuilder::new(&perlin)
-        .set_size(2048, 2048)
+fn gen_perlin_noise(width: usize, height: usize) -> NoiseMap {
+    let noise = Fbm::default();
+
+    PlaneMapBuilder::new(&noise)
+        .set_size(width, height)
         .set_x_bounds(-5., 5.)
         .set_y_bounds(-5., 5.)
         .build()
-        .write_to_file("sla.png");
+}
+
+fn gen_mesh(noise: NoiseMap) -> Mesh {
+    let (width, height) = noise.size();
+    let top_left_x = (width as f32 - 1.) / -2.;
+    let top_left_z = (height as f32 - 1.) / -2.;
+
+    let mut vertices = Vec::with_capacity(width * height);
+    let mut indices: Vec<u32> = Vec::with_capacity((width - 1) * (height - 1) * 6);
+
+    let mut vertex_index = 0_usize;
+
+    for x in 0..height {
+        for y in 0..width {
+            vertices.push([
+                top_left_x + x as f32,
+                noise.get_value(x, y) as f32,
+                top_left_z - y as f32,
+            ]);
+
+            if x < width - 1 && y < height - 1 {
+                indices.push(vertex_index as u32);
+                indices.push((vertex_index + width + 1) as u32);
+                indices.push((vertex_index + width) as u32);
+                indices.push((vertex_index + width + 1) as u32);
+                indices.push(vertex_index as u32);
+                indices.push((vertex_index + 1) as u32);
+            }
+
+            vertex_index += 1;
+        }
+    }
+
+    let mut mesh = Mesh::new(TriangleList);
+    mesh.set_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        VertexAttributeValues::Float32x3(vertices),
+    );
+    mesh.set_indices(Some(Indices::U32(indices)));
+
+    mesh
+}
+
+pub fn gen_terrain(
+    mut command: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let noise_map = gen_perlin_noise(256, 256);
+    let mesh = gen_mesh(noise_map);
+
+    command
+        .spawn()
+        .insert(Terrain)
+        .insert(Transform::from_xyz(0., 0., 0.))
+        .insert_bundle(PbrBundle {
+            mesh: meshes.add(mesh),
+            material: materials.add(StandardMaterial {
+                ..Default::default()
+            }),
+            ..Default::default()
+        });
 }
